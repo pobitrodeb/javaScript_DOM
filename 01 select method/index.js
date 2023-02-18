396838623a42a3253b7be36d739d309b9d297337
Pobitrodeb <pobitrodn@gmail.com>
2023-02-18 20:47:18 +0600

    getElementByClassName data catch

--- 01 select method/index.js
+++ 01 select method/index.js
@@ -1,2 +1,7 @@
 let heading =  document.getElementById('heding');
 heading.innerHTML = "Pobitro Debnath"; 
+
+
+let CardDesign = document.getElementsByClassName('CardDesign')[0]; 
+// [0] is class index number 
+CardDesign.innerHTML = "This card change by JS"; 
