b78e59c6a19c4f7ca722ac0c2f24fe64fa7bd4eb
Pobitrodeb <pobitrodn@gmail.com>
2023-02-18 21:05:55 +0600

    getElementByName session donee

--- 01 select method/index.js
+++ 01 select method/index.js
@@ -2,6 +2,19 @@ let heading =  document.getElementById('heding');
 heading.innerHTML = "Pobitro Debnath"; 
 
 
-let CardDesign = document.getElementsByClassName('CardDesign')[0]; 
+let CardDesign = document.getElementsByClassName('CardDesign')[2]; 
 // [0] is class index number 
 CardDesign.innerHTML = "This card change by JS"; 
+
+let paragraph = document.getElementsByName('thisIsParagraphTag')[0]; 
+paragraph.innerHTML = 'This is First Paragraph Tag '; 
+
+
+let paragraph1 = document.getElementsByName('thisIsParagraphTag')[1]; 
+paragraph1.innerHTML = 'This is Secound Paragraph Tag '; 
+
+let paragraph2 = document.getElementsByName('thisIsParagraphTag')[2]; 
+paragraph2.innerHTML = 'This is Secound Paragraph Tag '; 
+
+let paragraph3 = document.getElementsByName('thisIsParagraphTag')[3]; 
+paragraph3.innerHTML = 'This is Third Paragraph Tag '; 
