9b956b00f730a7022547b14e6a387155080bba98
Pobitrodeb <pobitrodn@gmail.com>
2023-02-19 21:55:40 +0600

    textContent added

--- 05 DOM Manipulation/index.js
+++ 05 DOM Manipulation/index.js
@@ -1,5 +1,15 @@
 let div = document.createElement('div');
 // div.id = "paragraphID";
 div.className = "paragraphClass";
+
 div.innerHTML = "<p> This is paragraph tag </p>";
+
+let text = document.createTextNode('Create Text Noede');
+div.appendChild(text);
+
+let h1 = document.createElement("strong"); 
+h1.textContent = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat rerum soluta at ea eos fugit vero similique temporibus. Dicta excepturi dolorum laborum deserunt libero perferendis tempora pariatur recusandae vel quam.';
+div.appendChild(h1); 
+
 document.body.appendChild(div);
+
