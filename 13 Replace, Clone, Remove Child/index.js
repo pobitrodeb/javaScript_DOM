67bde4c1edaf8bbc0bdfbad5da2977fdc1bedfd0
Pobitrodeb <pobitrodn@gmail.com>
2023-02-21 01:05:47 +0600

    lastChild added

--- 13 Replace, Clone, Remove Child/index.js
+++ 13 Replace, Clone, Remove Child/index.js
@@ -3,4 +3,5 @@ let menu = document.querySelector("#menu");
 let list = document.createElement("li"); 
 list.textContent = "Home", 
 
-menu.replaceChild(list, menu.firstChild)
+// menu.replaceChild(list, menu.firstChild);
+menu.replaceChild(list, menu.lastChild);
