7d80d5065a37d0106209bc8902dbbb95a153cc58
Pobitrodeb <pobitrodn@gmail.com>
2023-02-21 01:34:15 +0600

    Working With Attribute session done

--- 14 attributes/index.js
+++ 14 attributes/index.js
@@ -6,3 +6,11 @@ let btn = document.querySelector("#btn");
 if(btn){
     btn.setAttribute("name", "Pobitro");
 }
+
+// get attribute 
+
+let btn2 = document.querySelector("btn2"); 
+
+if(btn2){
+    console.log(btn2.getAttribute("target"))
+}
