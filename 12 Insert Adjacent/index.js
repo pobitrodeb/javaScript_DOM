5b8558739c7a4a5abff468f62ee2ed19e2aff45c
Pobitrodeb <pobitrodn@gmail.com>
2023-02-20 23:47:45 +0600

    Insert Adjacent session done

--- 12 Insert Adjacent/index.js
+++ 12 Insert Adjacent/index.js
@@ -3,3 +3,4 @@ let menu = document.querySelector("#menu");
 menu.insertAdjacentHTML("beforebegin", "<h1> Tthis is before begin  </h1>");
 menu.insertAdjacentHTML("afterbegin", "<h1> Tthis is after begin  </h1>");
 menu.insertAdjacentHTML("afterend", "<h1> Tthis is after end </h1>");
+menu.insertAdjacentHTML("beforeend", "<h1> Tthis is beforend end </h1>");
