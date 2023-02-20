093ea33ed037d918ddf6a0a60058283a91a98ae4
Pobitrodeb <pobitrodn@gmail.com>
2023-02-20 23:02:07 +0600

    Append & Prepend Method session done

--- 11 Append & Prepend Method/index.js
+++ 11 Append & Prepend Method/index.js
@@ -9,3 +9,16 @@ let createLanguage =  language.map((languageName) => {
 });
 
 app.append(...createLanguage);
+
+// console.log("Now we are see preppend");
+// Prepend Method
+
+let pre = document.querySelector("#apps"); 
+let frontEnd = ["JS", "BootStrap", "CSS"];
+let creteFontEndItem = frontEnd.map((frontEndLang) => {
+    let li = document.createElement("li"); 
+    li.textContent = frontEndLang; 
+    return li; 
+});
+
+apps.prepend(...creteFontEndItem);
