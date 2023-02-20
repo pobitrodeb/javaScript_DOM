b41799e312ccf2bd495c4a33ea60e1ab0e015d5a
Pobitrodeb <pobitrodn@gmail.com>
2023-02-20 23:44:16 +0600

    afterbegin added

--- 12 Insert Adjacent/index.js
+++ 12 Insert Adjacent/index.js
@@ -1,5 +1,4 @@
 let menu = document.querySelector("#menu"); 
 
 menu.insertAdjacentHTML("beforebegin", "<h1> Tthis is before begin  </h1>");
-
-menu.insertAdjacentHTML("beforebegin", "<h1> Tthis is before begin  </h1>");
+menu.insertAdjacentHTML("afterbegin", "<h1> Tthis is after begin  </h1>");
