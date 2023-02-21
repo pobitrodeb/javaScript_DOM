8b53b60c4fac99a8f39b5f1548752c6959341ada
Pobitrodeb <pobitrodn@gmail.com>
2023-02-21 13:51:09 +0600

    multiple style added

--- 15 Style Property/index.js
+++ 15 Style Property/index.js
@@ -1,4 +1,5 @@
 let text = document.querySelector("#article"); 
-text.style.background = "red";
-text.style.color      = "white";
-text.style.padding    = "15px";
+// text.style.background = "red";
+// text.style.color      = "white";
+// text.style.padding    = "15px";
+text.style.cssText = "background:green; color:white;"
