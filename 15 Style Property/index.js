a200703431b94a662ca0ee48b2d99cc55bdf57ee
Pobitrodeb <pobitrodn@gmail.com>
2023-02-21 13:59:17 +0600

    get property

--- 15 Style Property/index.js
+++ 15 Style Property/index.js
@@ -3,3 +3,8 @@ let text = document.querySelector("#article");
 // text.style.color      = "white";
 // text.style.padding    = "15px";
 text.style.cssText = "background:green; color:white;"
+
+// Get Computed Style Method
+let textTwo = document.querySelector(".textTwo");
+let style = getComputedStyle(textTwo); 
+console.log(style.fontSize);
