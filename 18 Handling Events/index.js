fa7c2714b21208983ff49b163fdeac5a7332beff
Pobitrodeb <pobitrodn@gmail.com>
2023-02-21 15:29:01 +0600

    event session done

--- 18 Handling Events/index.js
+++ 18 Handling Events/index.js
@@ -1,6 +1,24 @@
 let btn = document.querySelector("#btn"); 
 
 // DOM Level 0 event handlers
-btn.onclick = function() {
-    alert('Clicked!');
-};
+// btn.onclick = function() {
+//     alert('Clicked!');
+// };
+
+// btn.onclick = function() {
+//     alert(this.id); 
+// };
+
+// DOM Level 2 event handlers
+
+// btn.addEventListener('click',function(event) {
+//     alert(event.type); // click
+// });
+
+btn.addEventListener('click',function(event) {
+    alert('You clicked btton ');
+});
+// btn.addEventListener('click', showAlert);
+
+// remove the event listener
+// btn.removeEventListener('click', showAlert);
