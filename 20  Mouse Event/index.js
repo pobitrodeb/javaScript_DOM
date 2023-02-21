3c92ec143aae94ffe21031d50140f0ad3f4ec3b7
Pobitrodeb <pobitrodn@gmail.com>
2023-02-21 16:28:13 +0600

    Mouse Event sesson done

---
+++ 20  Mouse Event/index.js
@@ -0,0 +1,27 @@
+let mouseTrack = document.querySelector("#mouseTrack"); 
+
+mouseTrack.addEventListener("mousedown", (e) => {
+    console.log(e.type); 
+});
+
+
+
+mouseTrack.addEventListener("mouseup", (e) => {
+    console.log(e.type); 
+});
+
+
+mouseTrack.addEventListener("click", (e) => {
+    console.log(e.type); 
+});
+
+
+function mouseEvent(e){
+    console.log(e.type);
+}
+mouseTrack.addEventListener("dblclick", mouseEvent);
+
+// mouseTrack.addEventListener("mouseover", mouseEvent);
+// mouseTrack.addEventListener("mouseout", mouseEvent);
+// mouseTrack.addEventListener("mouseenter", mouseEvent);
+mouseTrack.addEventListener("mouseleave", mouseEvent);
