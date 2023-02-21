32adc377f224013199f816625c2a78b78fb4a8dc
Pobitrodeb <pobitrodn@gmail.com>
2023-02-21 14:54:31 +0600

    JavaScript Events session done

--- 17/index.js
+++ 17/index.js
@@ -1,4 +1,8 @@
-let btn = document.querySelector("#btn"); 
-btn.addEventListener("click", function(e){
-    alert("why you are click");
-}); 
+// let btn = document.querySelector("#btn"); 
+// btn.addEventListener("click", function(e){
+//     alert("why you are click");
+// }); 
+
+function onclicked(){
+    alert('clicked');
+}
