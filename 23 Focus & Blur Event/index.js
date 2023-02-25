8223548c71992de9905155d4591b128df15bb434
Pobitrodeb <pobitrodn@gmail.com>
2023-02-25 23:49:23 +0600

    blur session done

--- 23 Focus & Blur Event/index.js
+++ 23 Focus & Blur Event/index.js
@@ -3,3 +3,7 @@ textInput.addEventListener("focus", (e)=> {
    e.target.style.backgroundColor ="red",
    e.target.style.color ="white"
 })
+textInput.addEventListener("blur", (e)=> {
+   e.target.style.backgroundColor ="white",
+   e.target.style.color ="red"
+})
